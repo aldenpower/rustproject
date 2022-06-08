42c1839dca492e4a6cdb30026502f45f06b43889
aldenpower <anderson_afsl@hotmail.com>
2022-06-08 05:20:34 -0300

    add weight on mars program

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,24 +1,16 @@
 use std::io;
 
 fn main() {
+    println!("Enter your weight (kg)");
     let mut input = String::new();
 
-    io::stdin().read_line(&mut input);
+    io::stdin().read_line(&mut input).unwrap();
+    let weight: f32 = input.trim().parse().unwrap();
+    
+    //dbg!(weight);
 
-    borrow_string(&input);
-    own_string(input);
-
-    // let mars_weight: f32 = calculate_weight_on_mars(100.0);
-    // println!("Weight on Mars: {}kg", mars_weight);
-}
-
-
-fn borrow_string(s: &String) {
-    println!("{}", s);
-}
-
-fn own_string(s: String) {
-    println!("{}", s);
+    let mars_weight = calculate_weight_on_mars(weight);
+    println!("Weight on mars {mars_weight}kg")
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
