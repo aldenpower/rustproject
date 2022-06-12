a5d0d04540b3af51426935aea7fedad6001eb9c3
aldenpower <anderson_afsl@hotmail.com>
2022-06-12 17:50:52 -0300

    add git log in line

---
+++ binaries/gitcommits.rs
@@ -0,0 +1,36 @@
+/*Runs git log --oneline as an external Command and inspects its Output
+using Regex to get the hash and message of the last 5 commits.*/
+use std::process::Command;
+use regex::Regex;
+
+#[derive(PartialEq, Default, Clone, Debug)]
+struct Commit {
+    hash: String,
+    message: String,
+}
+
+fn main() -> Result<()> {
+    let output = Command::new("git").arg("log").arg("--oneline").output()?;
+
+    if !output.status.success() {
+        error_chain::bail!("Command executed with failing error code");
+    }
+
+    let pattern = Regex::new(r"(?x)
+                               ([0-9a-fA-F]+) # commit hash
+                               (.*)           # The commit message")?;
+
+    String::from_utf8(output.stdout)?
+        .lines()
+        .filter_map(|line| pattern.captures(line))
+        .map(|cap| {
+                 Commit {
+                     hash: cap[1].to_string(),
+                     message: cap[2].trim().to_string(),
+                 }
+             })
+        .take(5)
+        .for_each(|x| println!("{:?}", x));
+
+    Ok(())
+}
